0e291bdbd55bfd24e5d1f7472551ef2da5d39184
dasilvar <remi.dasilva@edu.esiee.fr>
2021-12-01 17:15:43 +0100

    ChangeDASH

--- WeatherDataFrame.py
+++ WeatherDataFrame.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def rename_date_frame_rows(data_frame: pd.DataFrame) -> pd.DataFrame:
+def rename_date_frame_rows(data_frame: pd.DataFrame) -> None:
     """
     Renomme les noms de colonnes de la DataFrame
     """
@@ -12,7 +12,7 @@ def rename_date_frame_rows(data_frame: pd.DataFrame) -> pd.DataFrame:
         inplace=True)
 
 
-def format_data_frame(data_frame: pd.DataFrame) -> pd.DataFrame:
+def format_data_frame(data_frame: pd.DataFrame) -> None:
     """
         Renomme les colonnes, supprime les valeurs à null et sélectionne uniquement les colonnes nécessaires :
         -commune-code
@@ -25,28 +25,9 @@ def format_data_frame(data_frame: pd.DataFrame) -> pd.DataFrame:
         -temp_C
         -mois
         """
-    data_frame = rename_date_frame_rows(data_frame)
+    rename_date_frame_rows(data_frame)
     return data_frame[['commune_code', 'commune_name', 'region_name', 'nom_dept', 'date',
                        'longitude', 'latitude', 'temp_C', 'mois']].dropna()
-
-
-def format_data_frame_groupby_commune(data_frame: pd.DataFrame) -> pd.DataFrame:
-    """
-    Formate la DataFrame en regroupant les données selon leur code de commune
-    """
-    data_frame = format_data_frame(data_frame)
-    return data_frame.groupby(['commune_code', 'commune_name', 'region_name', 'nom_dept',
-                               'longitude', 'latitude'], as_index=False).temp_C.mean()
-
-
-def format_data_frame_groupby_mois(data_frame: pd.DataFrame) -> pd.DataFrame:
-    """
-    Formate la DataFrame en regroupant les données selon leur code de commune
-    """
-    data_frame = format_data_frame(data_frame)
-    return data_frame.groupby(['commune_code', 'commune_name', 'region_name',
-                               'nom_dept', 'longitude', 'latitude', 'mois'],
-                              as_index=False).temp_C.mean()
 
 
 class WeatherDataFrame:
@@ -61,7 +42,11 @@ class WeatherDataFrame:
         """
         Renomme les noms de colonnes de la DataFrame
         """
-        rename_date_frame_rows(self.data_frame)
+        self.data_frame.rename(
+            columns={"communes (code)": "commune_code", "communes (name)": "commune_name",
+                     "region (name)": "region_name", "department (name)": "nom_dept", "Longitude": "longitude",
+                     "Latitude": "latitude", "Date": "date", "Température (°C)": "temp_C", "mois_de_l_annee": "mois"},
+            inplace=True)
 
     def format_data_frame(self) -> None:
         """
@@ -77,11 +62,39 @@ class WeatherDataFrame:
         -mois
         """
         self.rename_date_frame_rows()
-        format_data_frame(self.data_frame)
+        self.data_frame = self.data_frame[['commune_code', 'commune_name', 'region_name', 'nom_dept', 'date',
+                                           'longitude', 'latitude', 'temp_C', 'mois']].dropna()
+
+    def format_data_frame(self) -> None:
+        """
+        Renomme les colonnes, supprime les valeurs à null et sélectionne uniquement les colonnes nécessaires :
+        -commune-code
+        -commune-name
+        -region_name
+        -nom_dept
+        -date
+        -longitude
+        -latitude
+        -temp_C
+        -mois
+        """
+        self.rename_date_frame_rows()
+        self.data_frame = self.data_frame[['commune_code', 'commune_name', 'region_name', 'nom_dept', 'date',
+                                           'longitude', 'latitude', 'temp_C', 'mois']].dropna()
+
+    def format_data_frame_groupby_commune(self) -> None:
+        """
+        Formate la DataFrame en regroupant les données selon leur code de commune
+        """
+        self.format_data_frame()
+        self.data_frame = self.data_frame.groupby(['commune_code', 'commune_name', 'region_name', 'nom_dept',
+                                                   'longitude', 'latitude'], as_index=False).temp_C.mean()
 
     def format_data_frame_groupby_mois(self) -> None:
         """
         Formate la DataFrame en regroupant les données selon leur code de commune
         """
         self.format_data_frame()
-        format_data_frame_groupby_mois(self.data_frame)
+        self.data_frame = self.data_frame.groupby(['commune_code', 'commune_name', 'region_name',
+                                                   'nom_dept', 'longitude', 'latitude', 'mois'],
+                                                  as_index=False).temp_C.mean()
