d811300e50382d4413b5a375b2958efa06b1f2a1
dasilvar <remi.dasilva@edu.esiee.fr>
2021-12-27 21:42:46 +0100

    Suppr warning

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from Dashboard import Dashboard
 from WeatherDataFrame import WeatherDataFrame
-import warnings, os
 
-warnings.simplefilter("ignore") # Change the filter in this process
-os.environ["PYTHONWARNINGS"] = "ignore" # Also affect subprocesses
 weather_data_frame = WeatherDataFrame('donnees-synop-2021.csv')
 map_file_name = 'map.html'
 dashboard = Dashboard(weather_data_frame, map_file_name)
