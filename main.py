2ce8ebf8b7b760df26228677123e9a3cb658b7fe
dasilvar <remi.dasilva@edu.esiee.fr>
2021-12-27 21:31:19 +0100

    Change texte

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Dashboard import Dashboard
 from WeatherDataFrame import WeatherDataFrame
+import warnings
 
+warnings.filterwarnings("ignore")
 weather_data_frame = WeatherDataFrame('donnees-synop-2021.csv')
 
 map_file_name = 'map.html'
