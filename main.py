d7289c813206f1293cf8848679eb7eb7954cab1a
dasilvar <remi.dasilva@edu.esiee.fr>
2021-12-27 21:35:37 +0100

    Suppr warning

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from Dashboard import Dashboard
 from WeatherDataFrame import WeatherDataFrame
 import warnings
 
-warnings.filterwarnings("ignore")
-weather_data_frame = WeatherDataFrame('donnees-synop-2021.csv')
 
-map_file_name = 'map.html'
-dashboard = Dashboard(weather_data_frame, map_file_name)
-dashboard.show_dash()
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    weather_data_frame = WeatherDataFrame('donnees-synop-2021.csv')
+    map_file_name = 'map.html'
+    dashboard = Dashboard(weather_data_frame, map_file_name)
+    dashboard.show_dash()
