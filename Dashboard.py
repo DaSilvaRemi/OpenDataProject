89c80d9d4f71125d96c996be062b64a7521d6f4f
dasilvar <remi.dasilva@edu.esiee.fr>
2022-01-02 13:48:02 +0100

    End

--- Dashboard.py
+++ Dashboard.py
@@ -12,6 +12,7 @@ class Dashboard:
     """
     Classe gérant toutes les génération pour l'affichage du dashboard et affiche le dashboard.
     """
+
     def __init__(self, weather_data_frame: WeatherDataFrame, map_file_name: str = 'map.html') -> None:
         """
         Constructeur de la classe Dashboard
@@ -20,6 +21,8 @@ class Dashboard:
         -------
             weather_data_frame: DataFrame contenant les données météo
         """
+
+        # Enregistrement des dataframes
         self.app: dash.Dash = dash.Dash(__name__)
         self.map_file_name: str = map_file_name
         self.fig_data_frame: pandas.DataFrame = None
@@ -49,6 +52,8 @@ class Dashboard:
         """
         Créer une carte à partir de la DataFrame formatée pour la carte. Sauvegarde la map crée dans un fichier HTML.
         """
+
+        # Enregistrement des données nécessaire à l'affichage de la map
         latitude_values = self.map_data_frame['latitude'].values
         longitude_values = self.map_data_frame['longitude'].values
         temp_moy_values = self.map_data_frame['temp_C'].round(2).values
@@ -58,10 +63,12 @@ class Dashboard:
         center_cord = (46.539758, 2.430331)
         weather_map = folium.Map(location=center_cord, zoom_start=6)
 
+        # Coloration de la map
         color_map = branca.colormap.LinearColormap(colors=['blue', 'red'], vmin=min(temp_moy_values),
                                                    vmax=max(temp_moy_values))
         weather_map.add_child(color_map)
 
+        # Pour chaque données on créer un cercle
         for latitude, longitude, city, size, color in zip(latitude_values, longitude_values, city_values,
                                                           temp_moy_values, temp_moy_values):
             folium.CircleMarker(
@@ -85,6 +92,7 @@ class Dashboard:
             Un NumPy Histogram avec les données formaté dans un plotly bar
         """
 
+        #Compte le nombre de villes selon la température moyenne segmenter par intervalle
         counts_city, bins = np.histogram(self.map_data_frame["temp_C"], [-8, 0, 6, 11, 16, 21, 26, 31, 36, 41])
         bins = 0.5 * (bins[:-1] + bins[1:])
 
@@ -109,12 +117,15 @@ class Dashboard:
             Un Plotly Express Scatter avec les données formaté
         """
 
+        # Classe les températures par mois et par commune
         tmp_data_frame = self.fig_data_frame.query('mois >= ' + month_limit[0] +
                                                    ' and mois <=' + month_limit[1])
 
         tmp_data_frame = tmp_data_frame.groupby(['commune_code', 'commune_name', 'region_name',
                                                  'nom_dept', 'longitude', 'latitude', 'mois'],
                                                 as_index=False).temp_C.mean()
+
+        # Evite d'avoir des températures négatives causant des exceptions pour ce type de graph
         tmp_data_frame['size'] = tmp_data_frame['temp_C'].abs()
 
         return px.scatter(tmp_data_frame,
